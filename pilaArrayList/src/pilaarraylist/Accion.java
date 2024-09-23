e3f37c7829001562fd400097171e131b6923c9e5
Alexander Chacon <fresnedaalexander@gmail.com>
2024-09-23 00:56:32 -0500

    Carpintería
    Constructores y getters and setters de las clases Accion e HistorialCambios

--- pilaArrayList/src/pilaarraylist/Accion.java
+++ pilaArrayList/src/pilaarraylist/Accion.java
@@ -3,5 +3,36 @@ package pilaarraylist;
 
 
 public class Accion {
+    private String tipo;
+    private String descripcion;
+
+    public Accion() {
+    }
+
+    public Accion(String tipo, String descripcion) {
+        this.tipo = tipo;
+        this.descripcion = descripcion;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Accion{" + "tipo=" + tipo + ", descripcion=" + descripcion + '}';
+    }   
     
 }
--- pilaArrayList/src/pilaarraylist/HistorialCambios.java
+++ pilaArrayList/src/pilaarraylist/HistorialCambios.java
@@ -1,7 +1,34 @@
 
 package pilaarraylist;
 
+import java.util.LinkedList;
+import java.util.Stack;
+
 
 public class HistorialCambios {
+    
+    private Stack<LinkedList<Accion>> historial;
+
+    public HistorialCambios() {
+    }
+
+    public HistorialCambios(Stack<LinkedList<Accion>> historial) {
+        this.historial = historial;
+    }
+
+    public Stack<LinkedList<Accion>> getHistorial() {
+        return historial;
+    }
+
+    public void setHistorial(Stack<LinkedList<Accion>> historial) {
+        this.historial = historial;
+    }
+
+    @Override
+    public String toString() {
+        return "HistorialCambios{" + "historial=" + historial + '}';
+    }
+    
+    
     
 }
