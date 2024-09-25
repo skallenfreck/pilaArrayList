8b23a4577fc1febb79ccb05b11e695c1ffe39f57
Sharon Cruz <sharina.1401@gmail.com>
2024-09-24 21:05:14 -0500

    cambios metodos
    cambie realizar para q no reciba por teclado, mostrar y revertir

--- pilaArrayList/src/pilaarraylist/PilaArrayList.java
+++ pilaArrayList/src/pilaarraylist/PilaArrayList.java
@@ -1,64 +1,82 @@
 package pilaarraylist;
 
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class PilaArrayList {
-
-    static Scanner sc = new Scanner(System.in);
-
+    
     public static void main(String[] args) {
-        Stack<LinkedList<Accion>> historial = new Stack();
-        LinkedList<Accion> cambio = new LinkedList<>();
-
-        mostrarCambios(historial);
-
-        realizarCambios(cambio, historial);
+        Stack<ArrayList<Accion>> historial = new Stack();
         
+        realizarCambios(historial);
         mostrarCambios(historial);
+        System.out.println("");
         
-        mostrarCambios(historial);
-
-    }
-
-    public static void realizarCambios(LinkedList<Accion> cambio, Stack<LinkedList<Accion>> historial) {
-        Accion objAccion = new Accion();
-        System.out.println("Vamos a realizar cambios en el documento, entonces \n¿Cuál es el tipo de cambio que quiere realizar en el documento?");
-        String tipo = sc.nextLine();
-        objAccion.setTipo(tipo);
-
-        System.out.println("Describa el cambio que va a realizar");
-        String descripcion = sc.nextLine();
-        objAccion.setDescripcion(descripcion);
-        cambio.add(objAccion);
-        historial.push(cambio);
-    }
-
-    public static void mostrarCambios(Stack historial) {
         int i=0;
+        do{
+            revertirCambios(historial);
+            mostrarCambios(historial);
+            System.out.println("");
+            i++;
+        }while(i!=5);
+        
+    }
+
+    public static void realizarCambios(Stack<ArrayList<Accion>> historial) {
+        
+        ArrayList<Accion> cambio1 = new ArrayList<>();
+        Accion accion1 = new Accion("Cambio Multimedia", "Ajuste de imagen central.");
+        cambio1.add(accion1);
+        historial.push(cambio1);
+        
+        ArrayList<Accion> cambio2 = new ArrayList<>();
+        Accion accion2 = new Accion("Cambio Formato", "Ajuste de márgenes a 2cm");
+        cambio2.add(accion2);
+        historial.push(cambio2);
+        
+        ArrayList<Accion> cambio3 = new ArrayList<>();
+        Accion accion3 = new Accion("Cambio Tipografia", "Cambio de fuente a Arial");
+        cambio3.add(accion3);
+        historial.push(cambio3);
+        
+        ArrayList<Accion> cambio4 = new ArrayList<>();
+        Accion accion4 = new Accion("Correción", "Correción tildes");
+        cambio4.add(accion4);
+        historial.push(cambio4);
+        
+        ArrayList<Accion> cambio5 = new ArrayList<>();
+        Accion accion5 = new Accion("Cambio Tipografia", "Ajuste tamaño de fuente a 11");
+        cambio5.add(accion5);
+        historial.push(cambio5);
+        
+    }
+
+    public static void mostrarCambios(Stack<ArrayList<Accion>> historial) {
         if (historial.isEmpty()) {
             System.out.println("No se han realizado cambios en el documento");
-
         } else {
-
             System.out.println("Los cambios en el documento son:");
-            while (!historial.isEmpty()) {
-
-                System.out.println(i+"). "+historial.pop());
-                i++;
+            for(int i=0; i < historial.size(); i++){
+                ArrayList<Accion> cambio = historial.get(i);
+                for (int j=0; j<cambio.size(); j++) {
+                    Accion accion = cambio.get(j);
+                    System.out.println((i + 1) + "). " + accion);
+                }
             }
         }
     }
 
-    public static String revertirCambios(Stack historial){
-        mostrarCambios(historial);
-        System.out.println("¿Desea eliminar el primer cambio realizado? true/false");
-        Boolean v=sc.nextBoolean();
-        if (v) {
+    public static void revertirCambios(Stack<ArrayList<Accion>> historial){
+        if(!historial.isEmpty()){
+            ArrayList<Accion> ultimoCambio = historial.pop();
+            System.out.println("Se ha deshecho el ultimo cambio: ");
             
-            System.out.println("Se ha deshecho el cambio: "+);
+            for (int i = 0; i < ultimoCambio.size(); i++) {
+                Accion accion = ultimoCambio.get(i);
+                System.out.println(accion.revertir());
+            }
+        }else{
+            System.out.println("No hay cambios para deshacer.");
         }
-        return"";
     }
 }
